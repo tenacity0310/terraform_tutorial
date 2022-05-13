2dbef3e6845d05afb485a410a4b5fad9aac24c5b
Peter Li <thpeterli@gmail.com>
2022-05-13 10:39:52 +0800

    add terraform.tfstate adn and vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+# "aws_vpc": aws rescource vpc ----> name can't be changed
+# "peter_terraform": the logical name of the VPC on terrafrom to make reference in the future, that won't be showed on AWS
+resource "aws_vpc" "peter_terraform" {
+  cidr_block            = "10.87.0.0/16"
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
+  
+  tags = {
+# naming VPC in AWS Console
+    Name = "peter"
+  }
+}
