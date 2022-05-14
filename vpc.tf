8ea8033f600704444f85a4fec39be33b0eed1262
Peter Li <thpeterli@gmail.com>
2022-05-15 00:19:47 +0800

    add files for     internetgateway.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 # "aws_vpc": aws rescource vpc ----> name can't be changed
-# "peter_terraform": the logical name of the VPC on terrafrom to make reference in the future, that won't be showed on AWS
-resource "aws_vpc" "peter_terraform" {
-  cidr_block            = "10.87.0.0/16"
-  enable_dns_support    = true
-  enable_dns_hostnames  = true
-  
+# "peter_terraform_vpc": the logical name of the VPC on terrafrom to make reference in the future, that won't be showed on AWS
+resource "aws_vpc" "peter_terraform_vpc" {
+  cidr_block           = "10.87.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
-# naming VPC in AWS Console
-    Name = "peter"
+    # naming VPC in AWS Console
+    Name = "peter_vpc"
   }
 }
